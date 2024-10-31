fae399ae1b68dbd880d5461e6bdf80f86df20620
Abhay Nathwani <abhaynathwani123@gmail.com>
2024-10-31 07:09:35 +0530

    fixed firebase conn

--- tiktok_clone_app/lib/main.dart
+++ tiktok_clone_app/lib/main.dart
@@ -2,14 +2,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_clone_app/constants.dart';
+import 'package:tiktok_clone_app/controllers/auth_controller.dart';
 import 'package:tiktok_clone_app/views/screens/auth/signup_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized;
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  ).then((value) {
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
